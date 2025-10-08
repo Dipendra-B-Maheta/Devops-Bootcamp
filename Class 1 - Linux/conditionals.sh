c445ba0b4f171a6e0d4e7625d4211d3d91e4400b
Dipendra Maheta <dipendra.b.maheta@gmail.com>
2025-10-08 17:05:46 +0000

    class1

---
+++ Class 1 - Linux/conditionals.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# # ''
+
+# a=14
+# b=asdf
+# c=123asdf
+
+# echo $a
+# echo $b
+# echo $c
+
+# echo " a=$a b=$b c=$c"
+
+#2 - > User Input
+
+#!/bin/bash
+
+echo "What is your name?"
+read name
+echo "Hello, $name!"
+
+# 3 - > command line arguments
+
